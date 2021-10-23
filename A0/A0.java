4c5014b235306061279e452bf9a6009056896462
HussainNisyif <>
2021-10-23 15:54:19 +0000

    Implemented area calculator

--- A0/A0.java
+++ A0/A0.java
@@ -2,16 +2,15 @@
 
 
 */
-
+import java.lang.Math;
 
 public class A0{
 
 public static void main(String[] args){
 
   //Q1
-
-
-
+  double area = Math.PI * (15 * 15);
+  System.out.println("The area of this circle is " + area);
   //Q2
 
 
