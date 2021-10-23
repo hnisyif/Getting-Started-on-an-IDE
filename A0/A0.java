aa3119b3deb28eb4072352f2bf18cc00d3bc8cfd
Hussain <nisyh3327@wrdsb.ca>
2021-10-23 12:40:57 -0400

    Implemented Wayne Gretzkynumer

--- A0/A0.java
+++ A0/A0.java
@@ -1,5 +1,5 @@
 /*
-
+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
 
 */
 import java.lang.Math;
@@ -13,7 +13,9 @@ public static void main(String[] args){
   double area = Math.PI * (radius * radius);
   System.out.println("The area of this circle is " + area);
   //Q2
-  
+  String name = "Wayne Gretzky";
+  int num = 99;
+  System.out.println(name + " is number " + num);
 
 
 
